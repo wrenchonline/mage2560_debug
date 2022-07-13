e82d5a92746d3c3c5fae65d472cc334a9b1598cf
John Doe <ljl260435988@gmail.com>
2022-07-13 20:18:30 +0800

    test2

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,23 @@ void setup()
 {
   // initialize GDB stub
   debug_init();
-  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200); // 打开串口，并设置波特率为 9600 bps
 }
+
+int incomingByte = 0; // 定义变量用于接收串口数据
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+
+  // 检测缓冲区中是否有可读数据
+  if (Serial.available() > 0)
+  {
+    breakpoint();
+    // 有可读数据，读取1字节
+    incomingByte = Serial.read();
+
+    // 看看你收到的数据是什么
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
 }
